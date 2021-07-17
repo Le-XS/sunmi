83b1aaa9d53c4ab79466f6dd3d666550493ca82b
Le-XS <xiang_kiki@163.com>
2021-07-17 18:05:46 +0800

    添加代码

---
+++ Demo1/app/src/main/java/com/example/demo1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.ViewParent;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        List<Fragment> fragments = new ArrayList<>();
+    }
+}
